54348b487babb588e7644e3a542ea9d62c6db52a
jrvieira <github@jrvieira.com>
2020-04-05 01:38:49 +0100

    v

--- v.v
+++ v.v
@@ -16,10 +16,19 @@
 seven :int
  7
 
+function_name (Class constraint1) (Class constraint2) arg1:type arg2:type :return_type
+[match] # matches can be nested
+ result
+
+range (Ord A) f:A t:A :list A 
+ :list
+
+.syn .. range
+
 # comma (,) is equivalent to $ in haskell
 
 run :io nil
- print , seven arr:elem (:array 1 .. 7) # :true
+ print , seven arr:elem (:list 1 .. 7) # :true
  
 # types
 
@@ -77,16 +86,33 @@ fibs :list int
  0 : 1 : zip_with + fibs (tail fibs)
 
 # some more pattern matching and syntax
- ~ makes the match irrefutable
- ^ evaluates strictly
- comma allows breaking code into new line
+ 'match -> irrefutable match # ex. x':just 1
+ 'arg -> evaluates strictly # ex. seq 'x:A y:B :B 
+ matches can be nested
+
+# calculate the final direction after 2 turns
+direction one:dir two:dir :dir
+one:left
+ two:right
+  :front
+ two:left
+  :back
+...
 
 f l:list A :text # list char ?
-l~(^x : xx : xs)
+l '(x : xx : xs)
  "list: " ++ show l ++ "\n" , 
  "first: " ++ show x ,
  "second: " ++ show xx ,
  "rest: " ++ show xs
+
+# clauses
+
+fibs n:int :int
+ go 0 1 n
+ .
+ go 'a:int 'b:int n:int :int
+  n = 0 ? a , go b (a + b) (n - 1)
 
 if p:bool x:A y:A :A
 p:true
@@ -102,17 +128,25 @@ l\ # same as l:empty
 l:(x : xs) # same as l:cons x xs
  el = x ? :true , elem e xs 
 
-# more fun with guards
-
 ordering
  :lt
  :eq
  :gt
 
-order x:A y:a :ordering 
- x < y ? :lt ,
- x = y ? :eq ,
- :gt
+Ord A
+ max_bound :A
+ min_bound :A
+ succ x:A :A
+ pred x:A :A
+ lt x:A y:A :bool
+ gt x:A y:A :bool
+ compare x:A y:A :ordering
+  x < y ? :lt ,
+  x = y ? :eq ,
+  :gt
+
+.syn < 'lt
+.syn > 'gt
 
 # 'f = haskell `f`
 test :bool
